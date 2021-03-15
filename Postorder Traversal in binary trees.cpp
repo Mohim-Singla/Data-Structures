cdd0a046a71db8e78d89d3c6445de07f35a282dc
Mohim-Singla <mohimsingla@gmail.com>
2021-03-16 01:46:09 +0530

    Adding files

---
+++ Postorder Traversal in binary trees.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <stdlib.h>
+#include <queue>
+
+using namespace std;
+
+struct node{
+int data;
+node* lchild;
+node* rchild;
+};
+
+node* create_node(int data)
+{
+    node* temp=new node;
+    temp->data=data;
+    temp->lchild=temp->rchild=NULL;
+    return temp;
+}
+
+node* create(node* root, int data)
+{
+    if(root==NULL)
+    {
+        root = create_node(data);
+        return root;
+    }
+    else
+    {
+        if (data<root->data)
+        {
+            root->lchild = create(root->lchild,data);
+        }
+        else
+        {
+            root->rchild=create(root->rchild, data);
+        }
+        return root;
+    }
+}
+
+void postorder_traversal(node* root)
+{
+    if (root==NULL)
+    {
+        return;
+    }
+    else
+    {
+        if(root->lchild!=NULL)
+        {
+            postorder_traversal(root->lchild);
+        }
+
+        if(root->rchild!=NULL)
+        {
+            postorder_traversal(root->rchild);
+        }
+        cout<<root->data<<" ";
+    }
+}
+
+int main()
+{
+    node* root=NULL;
+    root=create(root, 16);
+    root=create(root, 4);
+    root=create(root, 10);
+    root=create(root, 20);
+    root=create(root, 11);
+    root=create(root, 25);
+    root=create(root, 8);
+    root=create(root, 6);
+    root=create(root, 14);
+    root=create(root, 2);
+    root=create(root, 21);
+    root=create(root, 31);
+    root=create(root, 7);
+    root=create(root, 32);
+
+    cout<<"preorder Traversal: ";
+    postorder_traversal(root);
+    return 0;
+}
